dd8c62bd21d3dd0bb39482df8be596c9d15e9edc
Mario Taddeucci <mariotaddeucci@gmx.com>
2025-02-10 08:38:24 -0300

    Publish items

--- src/whiscraper/__init__.py
+++ src/whiscraper/__init__.py
@@ -1,2 +1,5 @@
+from .browser.context import Browser as BrowserManager
 from .browser.context import BrowserManagerConfig as BrowserConfig
 from .browser.context import browser, get_page
+
+__all__ = ["BrowserConfig", "browser", "get_page", "BrowserManager"]
