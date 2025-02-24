b0d1654ba725f92cc933e758cd822f1522c7c4af
Mario Taddeucci <mariotaddeucci@gmx.com>
2025-02-23 23:14:35 -0300

    Better interceptor

--- src/whiscraper/browser/tools/request_interceptor.py
+++ src/whiscraper/browser/tools/request_interceptor.py
@@ -1,11 +1,22 @@
 import asyncio
 import fnmatch
 from collections import deque
-from dataclasses import dataclass
+from dataclasses import dataclass, field
+from datetime import datetime
 from functools import partial
 from typing import Callable, Tuple
 
 import nodriver
+
+
+@dataclass(frozen=True)
+class InterceptedResponse:
+    event: nodriver.cdp.network.ResponseReceived
+    intercepted_at: datetime = field(default_factory=datetime.now, init=False)
+
+    async def ensure_body_downloaded(self) -> None:
+        seconds_from_creation = (datetime.now() - self.intercepted_at).total_seconds()
+        await asyncio.sleep(max(2 - seconds_from_creation, 0))
 
 
 @dataclass(frozen=True)
@@ -20,107 +31,106 @@ class Response:
 class RequestInterceptor:
     def __init__(self, tab: nodriver.Tab):
         self._tab = tab
-        self._lock = asyncio.Lock()
-        self._debounce_task: asyncio.Task | None = None
-        self._debounce_event = asyncio.Event()
-        self._intercepted_responses: deque[nodriver.cdp.network.ResponseReceived] = deque()
-        self._patterns: list[str] = []
-        self._filters: list[Callable[[nodriver.cdp.network.ResponseReceived], bool]] = []
+        self._intercept_event = asyncio.Event()
+        self._intercepted_responses: deque[InterceptedResponse] = deque()
+        self._sniffers: list[Callable[[nodriver.cdp.network.ResponseReceived], bool]] = []
 
-    async def _debounce_event_trigger(self):
-        await asyncio.sleep(2)
-        async with self._lock:
-            self._debounce_event.set()
+    @staticmethod
+    def _sniffer_check(
+        event: nodriver.cdp.network.ResponseReceived,
+        patterns: list[str],
+        filter_fn: Callable[[nodriver.cdp.network.ResponseReceived], bool] | None,
+    ) -> bool:
+        if not any(fnmatch.fnmatch(event.response.url.lower(), pattern) for pattern in patterns):
+            return False
+        if filter_fn is None:
+            return True
+        return filter_fn(event)
 
-    async def _add_intercepted_response(self, response: nodriver.cdp.network.ResponseReceived):
-        async with self._lock:
-            self._intercepted_responses.append(response)
-            if self._debounce_task:
-                self._debounce_task.cancel()
-            self._debounce_event.clear()
-            self.task = asyncio.create_task(self._debounce_event_trigger())
-
-    async def _pop_intercepted_response(self, timeout: float = 10) -> nodriver.cdp.network.ResponseReceived:
-        async with asyncio.timeout(timeout):
-            await self._debounce_event.wait()
-        event = self._intercepted_responses.popleft()
-        if self.empty:
-            self._debounce_event.clear()
-        return event
-
-    def _match(self, url: str) -> bool:
-        return any(fnmatch.fnmatch(url.lower(), pattern) for pattern in self._patterns)
-
-    def sniff(self, *patterns: list[str] | str):
-        if len(self._patterns) == 0:
+    def sniff(
+        self,
+        *patterns: list[str] | str,
+        filter_fn: Callable[[nodriver.cdp.network.ResponseReceived], bool] | None = None,
+    ):
+        if len(self._sniffers) == 0:
             self._tab.add_handler(
                 nodriver.cdp.network.ResponseReceived,
                 self._cdp_receive_handler,
             )
 
-        for pattern in patterns:
-            if isinstance(pattern, list):
-                for p in pattern:
-                    self._patterns.append(p.lower())
-            else:
-                self._patterns.append(pattern.lower())
+        sniff_patterns = [
+            p.lower() for pattern in patterns for p in (pattern if isinstance(pattern, list) else [pattern])
+        ]
+
+        self._sniffers.append(partial(self._sniffer_check, patterns=sniff_patterns, filter_fn=filter_fn))
 
         return self
 
     async def _cdp_receive_handler(self, event: nodriver.cdp.network.ResponseReceived):
-        if not self._match(event.response.url):
+        if not any(sniffer(event) for sniffer in self._sniffers):
             return
 
-        if not all(fn(event) for fn in self._filters):
-            return
+        self._intercepted_responses.append(InterceptedResponse(event=event))
+        self._intercept_event.set()
 
-        await self._add_intercepted_response(event)
-
-    async def take(self, total: int, include_body: bool = True, timeout: float = 10, body_collect_timeout: int = 5):
+    async def take(self, total: int, include_body: bool = True, timeout: float = 10):
         for _ in range(total):
-            item = await self._pop_intercepted_response(timeout=timeout)
+            for _ in range(int(timeout + 1)):
+                if not self.empty:
+                    break
+                await asyncio.sleep(1)
+            else:
+                raise TimeoutError("Timeout waiting for intercepted responses")
+
+            intercepted_response = self._intercepted_responses.popleft()
+            await intercepted_response.ensure_body_downloaded()
+            event = intercepted_response.event
 
             resp_factory_fn = partial(
                 Response,
-                url=item.response.url,
-                request_id=str(item.request_id),
-                headers=dict(item.response.headers),
-                status_code=item.response.status,
+                url=event.response.url,
+                request_id=str(event.request_id),
+                headers=dict(event.response.headers),
+                status_code=event.response.status,
             )
 
-            if not include_body or item.response.status == 204:
+            if not include_body or event.response.status == 204:
                 yield resp_factory_fn(body=None)
                 return
 
-            cdp_command = nodriver.cdp.network.get_response_body(item.request_id)
+            cdp_command = nodriver.cdp.network.get_response_body(event.request_id)
+            response_body: Tuple[str, bool] | None = await self._tab.send(cdp_command)
 
-            for i in range(body_collect_timeout):
-                response_body: Tuple[str, bool] | None = await self._tab.send(cdp_command)
-                if response_body is not None and response_body[0].strip():
-                    break
-                elif i < body_collect_timeout - 1:
-                    await asyncio.sleep(1)
-            else:
+            if response_body is None:
                 yield resp_factory_fn(body=None)
                 return
 
             body_text, is_base64_encoded = response_body
-
             if is_base64_encoded:
                 body_text = body_text.encode("utf-8").decode("base64")
 
             yield resp_factory_fn(body=body_text)
 
-    async def get(self, include_body: bool = True, timeout: float = 10, body_collect_timeout: int = 5):
-        async for vl in self.take(
-            1, include_body=include_body, timeout=timeout, body_collect_timeout=body_collect_timeout
-        ):
+    async def get(self, include_body: bool = True, timeout: float = 10):
+        async for vl in self.take(1, include_body=include_body, timeout=timeout):
             return vl
 
-    def filter(self, fn: Callable[[nodriver.cdp.network.ResponseReceived], bool]):
-        self._filters.append(fn)
-        return self
+    async def get_all(self, include_body: bool = True, timeout: float = 10):
+        while not self.empty:
+            vl = await self.get(include_body=include_body, timeout=timeout)
+            yield vl
 
     @property
     def empty(self) -> bool:
         return len(self._intercepted_responses) == 0
+
+    def clear(self) -> None:
+        self._intercepted_responses.clear()
+
+    def reset(self) -> None:
+        self.clear()
+        self._sniffers.clear()
+
+    async def wait(self):
+        await self._intercept_event.wait()
+        self._intercept_event.clear()
